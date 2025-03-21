24b5a501a8c916130c32bb4c7f5dae27324ce2c1
Faviun <lightbringerlal@gmail.com>
2025-03-21 20:53:00 +0200

    filter-2

--- src/components/shared/checkbox-filters-group.tsx
+++ src/components/shared/checkbox-filters-group.tsx
@@ -9,7 +9,7 @@ type Item = FilterChecboxProps;
 interface Props {
     title: string;
     items: Item[];
-    defaulItems?: Item[];
+    defaulItems: Item[];
     limit?: number;
     searchInputPlaceholder?: string;
     onchange?: (value: string) => void;
@@ -27,17 +27,33 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
     defaultValue,
     className,
 }) => {
+    const [showAll, setShowAll] = React.useState(false);
+    const [searchValue, setSearchValue] = React.useState("");
+
+    const list = showAll
+        ? items.filter((item) =>
+              item.text.toLowerCase().includes(searchValue.toLowerCase())
+          )
+        : defaulItems.slice(0, limit);
+
+    const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchValue(e.target.value);
+    };
+
     return (
         <div className={className}>
             <p className="font-bold mb-3">{title}</p>
-            <div className="mb-5">
-                <Input
-                    placeholder={searchInputPlaceholder}
-                    className="bg-gray-50 border-none"
-                />
-            </div>
+            {showAll && (
+                <div className="mb-5">
+                    <Input
+                        onChange={onChangeSearchInput}
+                        placeholder={searchInputPlaceholder}
+                        className="bg-gray-50 border-none"
+                    />
+                </div>
+            )}
             <div className="flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar">
-                {items.map((item, index) => (
+                {list.map((item, index) => (
                     <FilterCheckbox
                         key={index}
                         onCheckedChange={() => console.log("checked")}
@@ -48,6 +64,20 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
                     />
                 ))}
             </div>
+            {items.length > limit && (
+                <div
+                    className={
+                        showAll ? "border-t border-t-neutral-100 mt-4" : ""
+                    }
+                >
+                    <button
+                        onClick={() => setShowAll(!showAll)}
+                        className="text-primary mt-3"
+                    >
+                        {showAll ? "Скрыть" : "+ Показать все"}
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
