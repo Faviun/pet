9d21284942d1d1f0fa2eaebc0f89445a7d57a22e
Faviun <lightbringerlal@gmail.com>
2025-03-27 21:10:21 +0200

    filters-query

--- prisma/constants.ts
+++ prisma/constants.ts
@@ -7,13 +7,22 @@ export const categories = [
 ]
 
 export const ingredients = [
-    {name: 'Помидоры', price: 10, imageUrl: 'https://cdn.pixabay.com/photo/2016/08/26/15/12/tomatoes-1621813_960_720.jpg'},
-    {name: 'Огурцы', price: 10, imageUrl: 'https://cdn.pixabay.com/photo/2016/08/26/15/12/tomatoes-1621813_960_720.jpg'},
-    {name: 'Сыр', price: 10, imageUrl: 'https://cdn.pixabay.com/photo/2016/08/26/15/12/tomatoes-1621813_960_720.jpg'},
+    {name: 'Помидоры', price: 10, imageUrl: '/images/cake.jpg'},
+    {name: 'Огурцы', price: 10, imageUrl: '/images/cake.jpg'},
+    {name: 'Сыр', price: 10, imageUrl: '/images/cake.jpg'},
+    {name: 'Хлеб', price: 10, imageUrl: '/images/cake.jpg'},
+    {name: 'Мясо', price: 10, imageUrl: '/images/cake.jpg'},
+    {name: 'Рыба', price: 10, imageUrl: '/images/cake.jpg'},
+    {name: 'Халва', price: 10, imageUrl: '/images/cake.jpg'},
+    {name: 'Картошка', price: 10, imageUrl: '/images/cake.jpg'},
+    {name: 'Макарошка', price: 10, imageUrl: '/images/cake.jpg'},
+    {name: 'Чай', price: 10, imageUrl: '/images/cake.jpg'},
+    {name: 'Арбузы', price: 10, imageUrl: '/images/cake.jpg'},
+
 ].map((ingredient, index) => ({id: index + 1, ...ingredient}))
 
 export const products = [
-    {name: 'Маргарита1', imageUrl: 'https://cdn.pixabay.com/photo/2016/08/26/15/12/tomatoes-1621813_960_720.jpg', categoryId: 2},
-    {name: 'Пепперони1', imageUrl: 'https://cdn.pixabay.com/photo/2016/08/26/15/12/tomatoes-1621813_960_720.jpg', categoryId: 3},
-    {name: 'Четыре сыра1', imageUrl: 'https://cdn.pixabay.com/photo/2016/08/26/15/12/tomatoes-1621813_960_720.jpg', categoryId: 4},
+    {name: 'Маргарита1', imageUrl: '/images/cake.jpg', categoryId: 2},
+    {name: 'Пепперони1', imageUrl: '/images/cake.jpg', categoryId: 3},
+    {name: 'Четыре сыра1', imageUrl: '/images/cake.jpg', categoryId: 4},
 ]
--- src/components/shared/checkbox-filters-group.tsx
+++ src/components/shared/checkbox-filters-group.tsx
@@ -2,18 +2,21 @@
 
 import React from "react";
 import {FilterChecboxProps, FilterCheckbox} from "./filter-checkbox";
-import {Input} from "../ui";
+import {Input, Skeleton} from "../ui";
 
 type Item = FilterChecboxProps;
 
 interface Props {
     title: string;
     items: Item[];
-    defaulItems: Item[];
+    defaulItems?: Item[];
     limit?: number;
+    loading?: boolean;
     searchInputPlaceholder?: string;
-    onchange?: (value: string) => void;
+    onCliclCheckbox?: (id: string) => void;
     defaultValue?: string[];
+    selected?: Set<string>;
+    name?: string;
     className?: string;
 }
 
@@ -22,19 +25,40 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
     items,
     defaulItems,
     limit = 5,
+    loading,
     searchInputPlaceholder = "Поиск...",
-    onchange,
+    onCliclCheckbox,
     defaultValue,
+    selected,
+    name,
     className,
 }) => {
     const [showAll, setShowAll] = React.useState(false);
     const [searchValue, setSearchValue] = React.useState("");
 
+    if (loading) {
+        return (
+            <div className={className}>
+                <p className="font-bold mb-3">{title}</p>
+
+                {...Array(limit)
+                    .fill(0)
+                    .map((_, index) => (
+                        <Skeleton
+                            key={index}
+                            className="h-6 mb-4 rounded-[8px]"
+                        />
+                    ))}
+                <Skeleton className="w-28 h-6 mb-4 rounded-[8px]" />
+            </div>
+        );
+    }
+
     const list = showAll
         ? items.filter((item) =>
               item.text.toLowerCase().includes(searchValue.toLowerCase())
           )
-        : defaulItems.slice(0, limit);
+        : (defaulItems || items).slice(0, limit);
 
     const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setSearchValue(e.target.value);
@@ -56,11 +80,12 @@ export const CheckboxFiltersGroup: React.FC<Props> = ({
                 {list.map((item, index) => (
                     <FilterCheckbox
                         key={index}
-                        onCheckedChange={() => console.log("checked")}
-                        checked={false}
+                        onCheckedChange={() => onCliclCheckbox?.(item.value)}
+                        checked={selected?.has(item.value)}
                         value={item.value}
                         text={item.text}
                         endAdornment={item.endAdornment}
+                        name={name}
                     />
                 ))}
             </div>
